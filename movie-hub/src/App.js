c244ad856d02dafc02c01a99f1149aaa6c926585
Omkar-Vilankar <79637894+Omkar-Vilankar@users.noreply.github.com>
2022-02-05 12:05:29 +0530

    Update App.js

--- movie-hub/src/App.js
+++ movie-hub/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React omkar vilankar1
+          Learn React omkar vilankar
         </a>
       </header>
     </div>
